5dbc684fdfd4b633472c9c129563005c4f6e3168
André Jacobs <jacobs.andre9@gmail.com>
2017-05-13 18:40:06 +0200

    Rest of main

--- main.c
+++ main.c
@@ -23,9 +23,22 @@ int main( int argc, char **argv )
 
         static char* provided_password = "init";
         static char* new_password = "init2";
+
 	if ( SGX_SUCCESS != (ret = set_password( eid, &output, provided_password, new_password ) ) )
 		printf( "Error calling enclave\n (error 0x%x)\n", ret );
 	else
 		printf( "%s −> %s\n", provided_password, new_password );
 	
+
+        static char* new_secret = "secret";
+	if ( SGX_SUCCESS != (ret = set_secret( eid, &output, new_password, new_secret ) ) )
+		printf( "Error calling enclave\n (error 0x%x)\n", ret );
+	else
+		printf( "Succesfully set secret set to: ", new_secret );
+
+        char* secret;
+	if ( SGX_SUCCESS != (ret = get_secret( eid, &output, new_password, secret ) ) )
+		printf( "Error calling enclave\n (error 0x%x)\n", ret );
+	else
+		printf( "Secret is: ", secret );
 }
